9db96066c221956e142590e580ffa80f229f50d4
Kapitonov Roman <kapitroman@yandex.ru>
2020-09-02 18:51:37 +0300

    Учебный проект: большие перемены (Часть 1)

--- src/view/load-more-button.js
+++ src/view/load-more-button.js
@@ -7,6 +7,7 @@ const createLoadMoreButtonTemplate = () => {
 export default class LoadMoreButton extends AbstractView {
   constructor() {
     super();
+
     this._clickHandler = this._clickHandler.bind(this);
   }
 
@@ -16,11 +17,14 @@ export default class LoadMoreButton extends AbstractView {
 
   _clickHandler(evt) {
     evt.preventDefault();
+
     this._callback.click();
   }
 
   setClickHandler(callback) {
+
     this._callback.click = callback;
+
     this.getElement().addEventListener(`click`, this._clickHandler);
   }
 }
