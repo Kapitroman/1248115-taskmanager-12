97383e6c3a1dd66fa94bd52e3f665d43827a7302
Kapitonov Roman <kapitroman@yandex.ru>
2020-09-10 19:46:18 +0300

    Учебный проект: меняй-удаляй (часть 1)

--- src/view/site-menu.js
+++ src/view/site-menu.js
@@ -1,5 +1,7 @@
 import AbstractView from "./abstract.js";
 
+// Функцию для генерации HTML-разметки можно превратить в метод класса,
+// однако делать мы этого не будем, чтобы не раздувать diff изменений
 const createSiteMenuTemplate = () => {
   return `<section class="control__btn-wrap">
     <input
